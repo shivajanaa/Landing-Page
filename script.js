410276ea9e3963328cd215d344afb4cbe13eb2f2
Shiva <sjana0484@gmail.com>
2025-03-10 17:53:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+
+
+function page1Animation(){
+
+
+ let tl = gsap.timeline();
+tl.from("nav h1 , nav a ,nav .btn1",{
+    y: -40,
+    delay:0.5,
+    stagger:0.1,
+    opacity:0,
+})
+
+tl.from(".contentP1  h1,.contentP1 p, .contentP1 .btn2 ", {
+    x:-290,
+    stagger:0.1,
+    opacity:0,
+})
+tl.from(".contentP2 .image1",{
+   
+    opacity:0,
+    stagger:0.2,
+})
+
+
+
+
+
+function animateSection(section) {
+    gsap.from(`${section} h1, ${section} p`, {
+        y: 20,
+        opacity: 0,
+        delay: 0.2,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: `${section} h1`,
+            scroller: "body",
+            start: "top 80%"
+        }
+    });
+}
+
+animateSection(".section2");
+animateSection(".section4");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+page1Animation();
+
+
+
+
+
+
+
